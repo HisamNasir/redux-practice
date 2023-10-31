1d3bbdc8cfefc153d42d788040db042deb3f7021
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2023-10-31 16:35:56 +0500

    done

--- components/OrderHistoryItem.jsx
+++ components/OrderHistoryItem.jsx
@@ -1,5 +1,21 @@
+<<<<<<< HEAD
 const ProductItem = ({ productId }) => {
   const [product, setProduct] = useState(null);
+=======
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const OrderHistoryItem = ({ productId }) => {
+const [product, setProduct] = useState(null);
+  useEffect(() => {
+    const fetchProductDetails = async () => {
+      try {
+        const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching product details", error);
+      }
+    };
+>>>>>>> parent of 18f28f6 (done)
 
   useEffect(() => {
     // Fetch the product details from the 'products' collection
@@ -13,8 +29,19 @@ const ProductItem = ({ productId }) => {
 
   return (
     <li>
+<<<<<<< HEAD
       {product ? product.title : "Product not found"}
       {/* You can display other product information here */}
+=======
+      {product ? (
+        <div>
+          <h3>{product.title}</h3>
+          <p>Price: ${product.price}</p>
+        </div>
+      ) : (
+        <p>Loading product details...</p>
+      )}
+>>>>>>> parent of 18f28f6 (done)
     </li>
   );
 };
--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -4,8 +4,12 @@ import ProductCard from "@/components/ProductCard";
 import { useDispatch } from "react-redux";
 import Layout from "@/components/Layout";
 import CartFooter from "@/components/CartFooter";
+<<<<<<< HEAD
 import ProductSearchBar from "@/components/ProductSearchBar"; 
 import ProtectedPage from "@/components/ProtectedPage";
+=======
+
+>>>>>>> parent of 18f28f6 (done)
 const HomePage = () => {
   const [products, setProducts] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
@@ -23,8 +27,11 @@ const HomePage = () => {
   return (
     <ProtectedPage>
       <Layout>
+<<<<<<< HEAD
         <div className="mb-4">
         </div>
+=======
+>>>>>>> parent of 18f28f6 (done)
         <div className="bg-gray-100 space-y-2 rounded-lg dark:bg-gray-900 p-4">
           <h1 className="text-2xl font-semibold text-gray-800 dark:text-gray-200">
             Products
